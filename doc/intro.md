34db37f99abbf31bb0f0150a3339e2529b08864d
MartinPuda <mapuda@seznam.cz>
2023-06-21 16:09:50 +0200

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljfx-tower
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cljfx_tower/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljfx-tower.core-test
+  (:require [clojure.test :refer :all]
+            [cljfx-tower.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
